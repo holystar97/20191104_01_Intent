0d95cbb50766da509304af5d0083a09b8563d88e
holystar97 <mkrice500@gmail.com>
2019-11-04 19:54:32 +0900

    화면이동- startActivity For Result 취소버튼- finish 화면 종료

--- app/src/main/java/com/tjoeun/a20191104_01_intent/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191104_01_intent/MainActivity.kt
@@ -2,11 +2,22 @@ package com.tjoeun.a20191104_01_intent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        nameInputBtn.setOnClickListener {
+            var intent=Intent(this,EditUserInfoActivity::class.java)
+            startActivityForResult(intent,1000)
+
+
+
+        }
+
     }
 }
